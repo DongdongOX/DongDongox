4410fd8f0321dfcdefa2a5efc94fca62050ca20e
xiaoyiyi <626241361@qq.com>
2025-11-24 16:48:55 +0800

    [add]add my development board

--- uboot/compile.sh
+++ uboot/compile.sh
@@ -1,2 +1,2 @@
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_xyy_emmc_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
