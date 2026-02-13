7ece40a0647906c0dd2015974869cce0beb9fe48
xiaoyiyi <626241361@qq.com>
2026-02-13 14:18:36 +0800

    add doc of mx6ull_xyy_emmc

--- UBOOT/complish.sh
+++ UBOOT/complish.sh
@@ -1,3 +1,3 @@
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_xyy_emmc_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12
